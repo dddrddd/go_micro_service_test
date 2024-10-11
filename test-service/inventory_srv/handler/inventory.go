5735b1067aa7eba77d257281418fb0078d1e8450
drd <359498009@qq.com>
2024-10-11 14:32:50 +0800

    库存2

--- test-service/inventory_srv/handler/inventory.go
+++ test-service/inventory_srv/handler/inventory.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"context"
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"sync"
 	"test-service/inventory_srv/global"
 	"test-service/inventory_srv/model"
 	"test-service/inventory_srv/proto"
@@ -37,23 +39,37 @@ func (*InventoryServer) InvDetail(c context.Context, req *proto.GoodsInvInfo) (*
 
 }
 
+var m sync.Mutex
+
 func (*InventoryServer) Sell(c context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
-	//todo 处理并发场景
 	tx := global.DB.Begin()
+	//m.Lock()
 	for _, good := range req.GoodsInfo {
 		var inv model.Inventory
-		if result := global.DB.Where(&model.Inventory{Goods: good.GoodsId}).First(&inv); result.RowsAffected == 0 {
-			tx.Rollback()
-			return nil, status.Errorf(codes.InvalidArgument, "库存信息不存在")
+		//if result := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where(&model.Inventory{Goods: good.GoodsId}).First(&inv); result.RowsAffected == 0 {
+		//	tx.Rollback()
+		//	return nil, status.Errorf(codes.InvalidArgument, "库存信息不存在")
+		//}
+		for {
+			if result := global.DB.Where(&model.Inventory{Goods: good.GoodsId}).First(&inv); result.RowsAffected == 0 {
+				tx.Rollback()
+				return nil, status.Errorf(codes.InvalidArgument, "库存信息不存在")
+			}
+			if inv.Stocks < good.Number {
+				tx.Rollback()
+				return nil, status.Errorf(codes.ResourceExhausted, "库存不足")
+			}
+			inv.Stocks -= good.Number
+			if result := tx.Model(&model.Inventory{}).Select("Stocks", "Version").Where("goods = ? and version = ?", good.GoodsId, inv.Version).Updates(model.Inventory{Stocks: inv.Stocks, Version: inv.Version + 1}); result.RowsAffected == 0 {
+				zap.S().Info("库存扣减失败")
+			} else {
+				break
+			}
 		}
-		if inv.Stocks < good.Number {
-			tx.Rollback()
-			return nil, status.Errorf(codes.ResourceExhausted, "库存不足")
-		}
-		inv.Stocks -= good.Number
-		tx.Save(&inv)
+		//tx.Save(&inv)
 	}
 	tx.Commit()
+	//m.Unlock()
 	return &emptypb.Empty{}, nil
 }
 
